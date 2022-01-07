72897cb4ba079b51fd3a2d6115ea9784a20e0c2f
Vijaya <vijayajhala@gmail.com>
2022-01-07 13:49:35 +0530

    Initial commit

---
+++ components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header';
+
+const layout = ({children}) => {
+    return (
+        <>
+        <Header />
+        {children}
+      </>
+    )
+}
+
+export default layout
+
